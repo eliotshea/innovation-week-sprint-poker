4e2d99f6ae19e98346b99a80b5a83125e719552b
Eliot Shea <eliot.shea@gmail.com>
2024-07-26 12:44:08 -0400

    add env changes to vercel kv

--- src/env.js
+++ src/env.js
@@ -22,6 +22,10 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url()
     ),
+    KV_URL: z.string().url(),
+    KV_REST_API_URL: z.string().url(),
+    KV_REST_API_TOKEN: z.string(),
+    KV_REST_API_READ_ONLY_TOKEN: z.string(),
   },
 
   /**
@@ -42,6 +46,10 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    KV_URL: process.env.KV_URL,
+    KV_REST_API_URL:process.env.KV_REST_API_URL,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
