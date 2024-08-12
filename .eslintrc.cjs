a69cecb7db466837671d09c07c5623f82f98ddbd
Eliot Shea <eliot.shea@gmail.com>
2024-08-12 12:27:04 -0400

    fix build (lmao)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -39,6 +39,7 @@ const config = {
     ],
    "@typescript-eslint/no-explicit-any": "off",
    "@typescript-eslint/no-unsafe-member-access": "off",
+   "@typescript-eslint/no-unsafe-assignment": "off",
   }
 }
 module.exports = config;
