36a458b8f5f66cc985bf6f0c969523b3d64ce9da
Eliot Shea <eliot.shea@gmail.com>
2025-01-24 12:30:49 -0500

    usevotes hook

---
+++ src/hooks/useVotes.ts
@@ -0,0 +1,35 @@
+import { get } from "http";
+import { useState, useEffect } from "react";
+import { socket } from "~/pages/_app";
+import { Room } from "~/types/votes";
+
+const useRoomVotes = (roomId: string) => {
+  const [room, setRoom] = useState<Room>();
+
+  const getRoom = () => {
+    console.log("getRoom");
+    socket.emit("getvotes", { roomId });
+  };
+
+  useEffect(() => {
+    socket.on("getvotes", (value: Room) => {
+      setRoom(value);
+    });
+
+    return () => {
+      socket.off("getvotes");
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("focus", getRoom);
+
+    return () => {
+      window.removeEventListener("focus", getRoom);
+    };
+  }, [roomId]);
+
+  return { room, getRoom };
+};
+
+export default useRoomVotes;
