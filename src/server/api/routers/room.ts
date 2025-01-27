c106e7663dca354ad80d0f61f2e0410c360fbb87
Eliot Shea <eliot.shea@gmail.com>
2025-01-27 16:18:54 -0500

    refactor roomRouter to use kv for room storage and simplify room creation and joining logic

--- src/server/api/routers/room.ts
+++ src/server/api/routers/room.ts
@@ -1,24 +1,29 @@
+import { Room } from "~/types/room.schema";
+import { kv } from "@vercel/kv";
 import { z } from "zod";
-import {
-  createTRPCRouter,
-  publicProcedure,
-} from "~/server/api/trpc";
-import { createRoomSchema } from "~/types/createRoom";
-import { joinRoomSchema } from "~/types/joinRoom";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createRoomSchema } from "~/types/createRoom.schema";
+import { joinRoomSchema } from "~/types/joinRoom.schema";
 import { randomString } from "~/utils/util";
 
 export const roomRouter = createTRPCRouter({
   create: publicProcedure
     .input(createRoomSchema)
-    .mutation(async ({ input, ctx }) => {
+    .mutation(async ({ input }) => {
       try {
-        const room = await ctx.db.room.create({
-          data: {
-            leader: input.name,
-            id: randomString(8),
+        console.log("create room", input);
+        const roomId = randomString(8);
+        const room: Room = {
+          roomId: roomId,
+          leader: {
+            id: input.id,
+            name: input.name,
+            vote: null,
           },
-        });
-
+          members: [],
+        };
+        await kv.set<Room>(`room-${roomId}`, room, { ex: 60 * 60 * 2 });
+        console.log("room created", room);
         return {
           ...room,
           error: null,
@@ -35,13 +40,9 @@ export const roomRouter = createTRPCRouter({
   join: publicProcedure
     .input(joinRoomSchema)
     .mutation(async ({ input, ctx }) => {
-
       try {
-        const room = await ctx.db.room.findUnique({
-          where: {
-            id: input.room,
-          },
-        });
+        const roomId = input.roomId;
+        const room = await kv.get<Room>(`room-${roomId}`);
 
         if (!room) {
           return {
@@ -49,25 +50,9 @@ export const roomRouter = createTRPCRouter({
           };
         }
 
-        if (room.leader === input.name || room.members.includes(input.name)) {
-          return {
-            error: "This name is already in use in this room",
-          };
-        }
-
-        const newRoom = await ctx.db.room.update({
-          where: {
-            id: input.room,
-          },
-          data: {
-            members: {
-              push: input.name,
-            },
-          }});
-
         return {
-          ...newRoom,
-          error: null
+          ...room,
+          error: null,
         };
       } catch {
         return {
@@ -75,41 +60,4 @@ export const roomRouter = createTRPCRouter({
         };
       }
     }),
-  getRoom: publicProcedure
-    .input(z.string())
-    .query(async ({ input, ctx }) => {
-      if (!input) {
-        return {
-          id: null,
-          leader: null,
-          members: [],
-          error: "You must enter a room ID",
-        };
-      }
-
-      try {
-        const room = await ctx.db.room.findUnique({
-          where: {
-            id: input,
-          },
-        });
-
-        if (room) {
-          return {
-            ...room,
-            error: null,
-          };
-        } else {
-          throw new Error("Room not found");
-        }
-
-      } catch {
-        return {
-          id: null,
-          leader: null,
-          members: [],
-          error: "Failed to get room",
-        };
-      }
-    })
 });
