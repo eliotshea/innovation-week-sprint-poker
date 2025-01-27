9c64c12710226b2744e34f0478609b106e455ba9
Eliot Shea <eliot.shea@gmail.com>
2025-01-27 16:17:06 -0500

    rename to useUser

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { socket } from "../pages/_app";
+
+type User = {
+  id: string;
+  name: string;
+};
+
+const useUser = (roomId: string) => {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    const storedUser = sessionStorage.getItem("user");
+    if (storedUser) {
+      try {
+        setUser(JSON.parse(storedUser));
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user && roomId) {
+      socket.emit("join-room", { roomId, user });
+    }
+  }, [user, roomId]);
+
+  return user;
+};
+
+export default useUser;
