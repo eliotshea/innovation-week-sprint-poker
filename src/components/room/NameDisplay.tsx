ea06c3426fe72dd0080834efe3f1d866415247e6
Eliot Shea <eliot.shea@gmail.com>
2025-01-27 16:19:01 -0500

    update NameDisplay component to display user name and handle absence of user

--- src/components/room/NameDisplay.tsx
+++ src/components/room/NameDisplay.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { useRoomContext } from "./RoomProvider";
 
 const NameDisplay: React.FC = () => {
-  const { name } = useRoomContext();
+  const { user } = useRoomContext();
 
-  return <div className="text-white">Logged in as: {name}</div>;
+  if (!user) {
+    return null;
+  }
+
+  return <div className="text-white">Logged in as: {user.name}</div>;
 };
 
 export default NameDisplay;
